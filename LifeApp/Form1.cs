7f39b91b60b6796eb507085990e7d0818e2cf806
Shoon <tishinadw@gmail.com>
2023-10-01 22:04:51 +0300

    Добавьте файлы проекта.

---
+++ LifeApp/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeApp
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        private bool[,] field;
+        private int rows;
+        private int cols;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame()
+        {
+            if (timer1.Enabled)
+            {//признак активности таймера 
+                return;
+            }
+
+            numDensity.Enabled = false;
+            numResolution.Enabled = false;
+
+            resolution = (int)numResolution.Value;
+
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+
+            field = new bool[cols, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next((int)numDensity.Value) == 0;
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+            //graphics.FillRectangle(Brushes.Crimson, 0, 0, resolution, resolution); //отрисовывало пробный квадратик
+
+        }
+
+        private void NextGeneration()
+        {
+            graphics.Clear(Color.Black);
+
+            var newField = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighborsCount = CountNeighbors(x, y);
+
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighborsCount == 3)
+                    {
+                        newField[x, y] = true;
+                    }
+                    else if (hasLife && (neighborsCount < 2 || neighborsCount > 3))
+                    {
+                        newField[x, y] = false;
+                    }
+                    else {
+                        newField[x, y] = field[x, y];
+                    }
+
+                    if (hasLife)
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
+                }
+            }
+
+            field = newField;
+            pictureBox1.Refresh();
+        }
+
+        private int CountNeighbors(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+
+                    int col = (x + i+cols)%cols;
+                    int row = (y + j+rows) % rows;
+
+                    bool isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+                    if (hasLife && !isSelfChecking) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+            {
+                return;
+            }
+            timer1.Stop();
+
+            numDensity.Enabled = true;
+            numResolution.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NextGeneration();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+    }
+}
